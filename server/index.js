bf126b2d378ddd3124464f5b92934342adbe5172
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2024-09-17 03:20:24 +0600

    sign up function added

--- server/index.js
+++ server/index.js
@@ -36,8 +36,8 @@ const verifyToken = async (req, res, next) => {
   })
 }
 
-// const uri = "mongodb://localhost:27017";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crlsfbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb://localhost:27017";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crlsfbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 
